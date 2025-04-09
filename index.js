16b71ed652784ef967ad538ef085b09e03a0c0ad
Nessa-blip <vanessakarla775@gmail.com>
2025-04-08 22:46:25 -0300

    Update index.js
    𝔅𝔬𝔱𝔷𝔦𝔫 𝔡𝔞 𝔤𝔞𝔩𝔢𝔯𝔦𝔫𝔥𝔞

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
+const BotName = '𝔅𝔬𝔱𝔷𝔦𝔫 𝔡𝔞 𝔤𝔞𝔩𝔢𝔯𝔦𝔫𝔥𝔞'; 
+const instagram = 'whois.__nessa'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
